b8e535e97749dc9c5d54dc48c81bde54a377328b
Michał K <uyu1@O2.pl>
2020-10-29 17:26:07 +0100

    Update program.java

--- program.java
+++ program.java
@@ -1,5 +1,7 @@
 class Program {
  public static void main(String args[]){
   System.out.println("Witam");
+  BufferedReader obj = new BufferedReader(new InputStreamReader(System.in));
+  String str = obj.readLine();
  }
 }
